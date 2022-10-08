e2961e8eaedb4af7e9305058b93dc508abbbec31
rasulvh <rasulvh@code.edu.az>
2022-10-08 20:44:10 +0400

    task added

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DersdeEtdiklerimizinPraktikasi
+            //int a = 9;
+            //int b = a;
+            //b = 12;
+            //Console.WriteLine(a + "  " + b);
+
+            //int[] nums = { 2, 34, 12, 41, 4, 8 };
+            //int[] nums1 = nums;
+            //nums1[0] = 9;
+            //Console.WriteLine(nums[0] + "  " + nums1[0]);
+
+            //int num = 25;
+            //ShowNumber(num);
+            //Console.WriteLine(num);
+
+            //int num = 25;
+            //Console.WriteLine(num);
+            //num += 5;
+            //ShowNumber(num);
+            //Console.WriteLine(num);
+
+            //int num = 30;
+            //Console.WriteLine(num);
+            //num += 10;
+            //ShowNumber(num);
+            //num -= 5;
+            //Console.WriteLine(num);
+
+            //int num = 30;
+            //num += 5;
+            //ShowNumber(ref num);
+            //Console.WriteLine(num);
+
+            //int num;
+            //ShowNumber(out num);
+            //num += 10;
+            //Console.WriteLine(num);
+
+
+            //string name1 = "Rasul";
+            //name1 = "Ali";
+            //Console.WriteLine(name1);
+
+            //string name = "Rasul";
+            //string name1 = name;
+            //name1 = "Ali";
+            //Console.WriteLine(name);
+            //Console.WriteLine(name1);
+
+            //string name;
+            //Check(out name);
+            //Console.WriteLine(name);
+
+            //StringBuilder name = new StringBuilder();
+            //name.Append("Ras");
+            //name.Append("ul");
+            //Console.WriteLine(name);
+
+            //string name1 = "Ras";
+            //string name2 = "ul";
+            //string result = name1 + name2;
+            //Console.WriteLine(result);
+
+            //String name3 = new String("Vasif");
+            //Console.WriteLine(name3);
+
+            //string name = "Rasul";
+            //Console.WriteLine(name.Length);
+
+            //string name = "RasulHasanov";
+            //Console.WriteLine(name.Substring(2,8));
+
+            //string name = "        Rasul    ";
+            //Console.WriteLine(name.Trim());
+
+            //string name = String.Empty;
+            //name = "";
+
+            //string name = "Rasul";
+
+            //Console.WriteLine(name.StartsWith("ra"));
+
+            //Console.WriteLine(name.EndsWith("ul"));
+
+            //if (name.StartsWith("Ra"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+            //Console.WriteLine(name.ToLower());
+
+            //Console.WriteLine(name.ToUpper());
+
+            //string name = "      Rasul   ";
+
+            //name = name.ToUpper();
+            //if (name.Trim().ToLower().StartsWith("r"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+            //string name = "Rasul";
+            //Console.WriteLine(name.IndexOf("a"));
+
+            //string name = "Abdullah";
+            //Console.WriteLine(name.LastIndexOf("a"));
+
+            //string name = "Vasif";
+            //name = name.Replace("Vasif", "Rasul");
+            //Console.WriteLine($"Hello, {name}");
+
+            //string name = "Mehemmed";
+            //Console.WriteLine(name.Remove(4,2));
+
+            //string name = "R-a-s-u-l";
+            //var result = name.Split("-");
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //string[] names = { "Rasul", "Vasif", "Abdullah", "Mehemmed" };
+            //var result = string.Join("-", names);
+            //Console.WriteLine(result);
+
+            //string name = "Rasul";
+            //if (string.IsNullOrEmpty(name))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+            //string text = null;
+            //if (string.IsNullOrWhiteSpace(text))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+            #endregion
+
+            //string a = "Rasul";
+            //string b = (string)a.Clone();
+            //Console.WriteLine(b);
+
+            //string a = "Salam Rasul";
+            //string b = "Salam";
+            //Console.WriteLine(a.Contains(b));
+
+            //string a = "Rasul";
+            //string b = "Ali";
+            //Console.WriteLine(a.Equals(b));
+
+            //string a = "Rasul";
+            //string b = a.Insert(2, "-Salam-");
+            //Console.WriteLine(b);
+
+            //string a = "This is \n my world";
+            //Console.WriteLine(a);
+
+        }
+
+        #region DersdeEtdiklerimizinPraktikasi
+        //public static void Check(out string str)
+        //{
+        //    str = "Vasif";
+        //    str += " - Rasul";
+        //    Console.WriteLine(str);
+        //}
+
+        //public static void ShowNumber(ref int num)
+        //{
+        //    num += 5;
+        //    Console.WriteLine(num);
+        //}
+
+        //public static void ShowNumber(out int num)
+        //{
+        //    num = 45;
+        //    num += 5;
+        //    Console.WriteLine(num);
+        //}
+        #endregion
+    }
+}
